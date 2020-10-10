eb5a9899289905e31464233a0465a79fa9097090
Lucas Monteiro Inacio <lucas.inacio@restoque.com.br>
2020-10-09 21:59:22 -0300

    feat: update produto instructions

--- src/core/entities/produto.ts
+++ src/core/entities/produto.ts
@@ -2,8 +2,11 @@ export default interface Produto {
   id: string
   nome: string
   marca: string
+  // Valor pago pelo produto
   valorCusto: number
+  // Quantos uso rende
   rendimento: number
+  // Rendimento * Valor custo
   valorVenda: number
   ativo: boolean
 }
