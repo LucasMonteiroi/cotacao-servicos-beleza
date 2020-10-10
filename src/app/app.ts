322b595aa8a59101bf8b94ea77988ea8f43a4dd9
Lucas Monteiro Inacio <lucas.inacio@restoque.com.br>
2020-10-09 22:02:30 -0300

    feat: create routes for cotacao and produto

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import dotEnv from 'dotenv'
 import ClienteController from './controllers/clienteController'
+import CotacaoController from './controllers/cotacaoController'
+import ProdutoController from './controllers/produtoController'
 
 dotEnv.config()
 const PORT = 8080
@@ -14,6 +16,18 @@ app.put('/cliente/:id', new ClienteController().update)
 app.delete('/cliente/:id', new ClienteController().delete)
 app.get('/cliente/:id', new ClienteController().getById)
 app.get('/cliente', new ClienteController().getAll)
+
+app.post('/cotacao', new CotacaoController().create)
+app.put('/cotacao/:id', new CotacaoController().update)
+app.delete('/cotacao/:id', new CotacaoController().delete)
+app.get('/cotacao/:id', new CotacaoController().getById)
+app.get('/cotacao', new CotacaoController().getAll)
+
+app.post('/produto', new ProdutoController().create)
+app.put('/produto/:id', new ProdutoController().update)
+app.delete('/produto/:id', new ProdutoController().delete)
+app.get('/produto/:id', new ProdutoController().getById)
+app.get('/produto', new ProdutoController().getAll)
 
 app.listen(PORT, () => {
   console.log(`server started at http://localhost:${PORT}`)
